0419eb195cdb144b07999ba1defea8295f7afd58
yukkuri <reityan.sakura0104@gmail.com>
2026-02-02 21:40:15 +0900

    ignore

---
+++ src/index.temp.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Docker PHP開発環境</title>
+</head>
+
+<body>
+  <h1>Docker PHP開発環境</h1>
+  <p>Docker PHP開発環境へようこそ！</p>
+  <ul>
+    <li><a href="./info.php">PHP 情報</a></li>
+    <li><a href="http://localhost:8081" target="_blank">phpMyAdmin</a></li>
+    <li><a href="http://localhost:8025" target="_blank">MailHog</a></li>
+    <li><a href="./db.php">データベース接続テスト</a></li>
+    <li><a href="./mail.php">メール送信テスト</a></li>
+  </ul>
+</body>
+
+</html>
