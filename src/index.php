8c987c90ca7e77fe6769cffb0985ac447157d5e7
yukkuri <reityan.sakura0104@gmail.com>
2026-03-03 16:14:27 +0900

    ColorPicker

--- src/index.php
+++ src/index.php
@@ -4,16 +4,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php</title>
+    <title>php_colorPicker</title>
 </head>
-<?php 
-$text = "Hello World";
 
-echo "<h1>{$text}</h1>"
-
+<?php
+if (isset($_GET['colorInp'])) {
+    $bgcolor = $_GET["colorInp"];
+} else {
+    $bgcolor = "#ffffff";
+}
+echo ("<p>" . "$bgcolor" . "</p>");
 ?>
 
-<body>
-    
+<body style="
+background-color:<?php echo ($bgcolor) ?>">
+    <form action="" method="get">
+        <h2>Choose bgColor</h2>
+        <input type="color" name="colorInp" value="<?php echo($bgcolor); ?>">
+        <button type="submit">set Color</button>
+    </form>
 </body>
+
 </html>
