a7d7de1d5ae976fbcd70fadc8cf7c78364646cdf
yukkuri <reityan.sakura0104@gmail.com>
2026-03-05 22:14:56 +0900

    jsonで追加/削除を実装

--- src/index.php
+++ src/index.php
@@ -8,17 +8,32 @@ if (isset($_POST["username"])) {
     $date = $datemili->modify("+9hours")->format("Y/m/d H:i:s");
 
     $arr = [
-        "username" => $username,
-        "comment" => $comment,
-        "date" => $date
+        "ユーザー名" => $username,
+        "内容" => $comment,
+        "データ登録日時" => $date
     ];
     array_push($Arraydata, $arr);
     $writeJson = json_encode($Arraydata, JSON_UNESCAPED_UNICODE);
 
     file_put_contents("datas.json", $writeJson);
 
-    header("Location: ", $_SERVER["PHP_SELF"]);
-}
+    header("Location: " . $_SERVER["PHP_SELF"]);
+    exit;
+};
+
+if (isset($_POST["deleteIdx"])) {
+    $focusIdx = (int)$_POST["deleteIdx"];
+
+    if (isset($Arraydata[$focusIdx])) {
+        array_splice($Arraydata, $focusIdx, 1);
+        $writeJson = json_encode($Arraydata, JSON_UNESCAPED_UNICODE);
+
+        file_put_contents("datas.json", $writeJson);
+
+        header("Location: " . $_SERVER["PHP_SELF"]);
+        exit;
+    }
+};
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +43,44 @@ if (isset($_POST["username"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Json操作</title>
+
+    <style>
+        table {
+            border-collapse: collapse;
+            margin: 30px 0;
+        }
+
+        th {
+            border: 1px solid #000;
+            padding: 0 2em;
+        }
+
+        td {
+            border: 1px solid #000;
+            padding: 0 1em;
+            text-align: center;
+        }
+
+        td:nth-child(4) {
+            padding: 1px;
+        }
+
+        td form {
+            width: 100%;
+            padding: 0;
+        }
+
+        td button {
+            width: 100%;
+            background-color: #ffc3c3;
+            cursor: pointer;
+        }
+
+        td button:hover {
+            background-color: #d4a2a2;
+        }
+    </style>
+
 </head>
 
 <body>
@@ -46,14 +99,23 @@ if (isset($_POST["username"])) {
                     <?php foreach (array_keys($Arraydata[0]) as $key): ?>
                         <th><?php echo ($key) ?></th>
                     <?php endforeach ?>
+                    <th>操作</th>
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($Arraydata as $user): ?>
+                <?php foreach ($Arraydata as $index => $user): ?>
                     <tr>
                         <?php foreach ($user as $value): ?>
-                            <th><?php echo (htmlspecialchars($value)) ?></th>
+                            <td><?php echo (htmlspecialchars($value)) ?></td>
                         <?php endforeach ?>
+
+                        <td>
+                            <form action="" method="post" onsubmit="return confirm(`<?php echo ($user['ユーザー名'] . 'さんの' . $user['内容'] . 'を削除しますか？') ?>`)">
+                                <input type="hidden" name="deleteIdx" value="<?php echo ($index) ?>">
+                                <button type="submit">削除</button>
+                            </form>
+                        </td>
+
                     </tr>
                 <?php endforeach ?>
             </tbody>
